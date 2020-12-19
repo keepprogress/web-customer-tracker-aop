a12a677da57abf467921a7e1b61026b50ab2cfdd
getzch <keepprogress@gmail.com>
2020-12-19 15:33:28 +0800

    add @Before advice to display method we are calling

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.luv2code.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -11,22 +13,39 @@ import org.springframework.stereotype.Component;
 public class CRMLoggingAspect {
 	
 	// setup logger
-	private Logger mylogger = Logger.getLogger(getClass().getName());
+	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
-	@Pointcut("execution(* com.luv2code.springdemo.aspect.*.*.(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
 	// dot the same for service and dao
-	@Pointcut("execution(* com.luv2code.springdemo.service.*.*.(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*.(..))")
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
 	private void forAppFlow() {}
+	
 	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @Before: calling method " + theMethod);
+		
+		// display the argument method
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	// add@AfterReturning advice
 
