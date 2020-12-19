bc077b1d7bc0e38d66dcfb0f146c60d05ad18303
getzch <keepprogress@gmail.com>
2020-12-19 15:20:46 +0800

    Setup pointcut declarations

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,7 +14,18 @@ public class CRMLoggingAspect {
 	private Logger mylogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
+	@Pointcut("execution(* com.luv2code.springdemo.aspect.*.*.(..))")
+	private void forControllerPackage() {}
 	
+	// dot the same for service and dao
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*.(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*.(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
 	// add @Before advice
 	
 	// add@AfterReturning advice
